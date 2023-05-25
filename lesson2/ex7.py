0ab41522cbfe453cc89501e946d76d639aaf95f9
Grigorii_Shakhin <grigorii_shakhin@epam.com>
2023-05-25 19:20:08 +0200

    add ex7.py

---
+++ lesson2/ex7.py
@@ -0,0 +1,26 @@
+import requests
+
+payload_list = [{"method": "GET"}, {"method": "POST"}, {"method": "PUT"}, {"method": "DELETE"}, {"method": "PATCH"},
+                {"method": "HEAD"}]
+url = "https://playground.learnqa.ru/ajax/api/compare_query_type"
+
+response_one = requests.post(url)
+print(f"response = {response_one.text}; method GET; params: {None}")
+
+response_two = requests.head(url, params=payload_list[5])
+print(f"response = {response_two.text}; method GET; params: {payload_list[5]}")
+
+response_three = requests.put(url, data=payload_list[2])
+print(f"response = {response_three.text}; method GET; params: {payload_list[2]}")
+print()
+
+for data in payload_list:
+    response_four = requests.get(url, params=data)
+    print(f"response = {response_four.text}; method GET; params: {data}")
+    response_four = requests.post(url, data=data)
+    print(f"response = {response_four.text}; method POST; params: {data}")
+    response_four = requests.put(url, data=data)
+    print(f"response = {response_four.text}; method PUT; params: {data}")
+    response_four = requests.delete(url, data=data)
+    print(f"response = {response_four.text}; method DELETE; params: {data}")
+    print()
