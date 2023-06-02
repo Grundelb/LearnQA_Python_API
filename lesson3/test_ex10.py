3ff8ea77862157050c6458d98050e6b892cdb011
Grigorii_Shakhin <grigorii_shakhin@epam.com>
2023-06-02 16:31:13 +0200

    change test_ex10.py

--- lesson3/test_ex10.py
+++ lesson3/test_ex10.py
@@ -1,5 +1,5 @@
 def test_input_length():
-    user_input = input()
+    user_input = input("Set a phrase: ")
     expected_length_max_value = 14
     actual_length = len(user_input)
     assert actual_length <= expected_length_max_value, f"Input length is {actual_length}, expected maximum value is {expected_length_max_value}"
