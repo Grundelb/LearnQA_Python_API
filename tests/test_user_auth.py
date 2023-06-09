0a5e02541e68295adbbb983164ff23d57b3b8a81
Grigorii_Shakhin <grigorii_shakhin@epam.com>
2023-06-09 22:06:34 +0200

    change BaseCase, TestUserAuth

--- tests/test_user_auth.py
+++ tests/test_user_auth.py
@@ -1,3 +1,26 @@
+import pytest
+import requests
+from lib.base_case import BaseCase
 
-class TestUserAuth:
-    pass
+
+class TestUserAuth(BaseCase):
+    url = "https://playground.learnqa.ru/api/user/login"
+
+    exclude_params = [
+        ("no_cookie"),
+        ("no_token")
+    ]
+
+    def setup(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+        response = requests.post(self.url, data = data)
+
+        self.auth_sid = self.get_cookie(response, "auth_sid")
+        self.token = self.get_header(response, "x-csrf-token")
+        self.user_id_from_auth_method = self.get_json_value(response, "user_id")
+
+    def test_auth_user(self):
+        pass
