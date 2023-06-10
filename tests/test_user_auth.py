647f2d00e18b503b1b7b1c345d120dbd22d7c6c4
Grisha Shakhin <57902725+Grundelb@users.noreply.github.com>
2023-06-10 15:17:05 +0200

    add TestUserRegister

--- tests/test_user_auth.py
+++ tests/test_user_auth.py
@@ -1,26 +1,56 @@
 import pytest
 import requests
 from lib.base_case import BaseCase
-
+from lib.assertions import Assertions
 
 class TestUserAuth(BaseCase):
-    url = "https://playground.learnqa.ru/api/user/login"
+    url_login = "https://playground.learnqa.ru/api/user/login"
+    url_auth = "https://playground.learnqa.ru/api/user/auth"
 
     exclude_params = [
         ("no_cookie"),
         ("no_token")
     ]
 
-    def setup(self):
+    def setup_method(self):
         data = {
             'email': 'vinkotov@example.com',
             'password': '1234'
         }
-        response = requests.post(self.url, data = data)
+        response = requests.post(self.url_login, data = data)
 
         self.auth_sid = self.get_cookie(response, "auth_sid")
         self.token = self.get_header(response, "x-csrf-token")
         self.user_id_from_auth_method = self.get_json_value(response, "user_id")
 
     def test_auth_user(self):
-        pass
+        response = requests.get(self.url_auth,
+                                headers={'x-csrf-token': self.token},
+                                cookies={'auth_sid': self.auth_sid}
+                                )
+
+        Assertions.assert_json_value_by_name(
+            response,
+            "user_id",
+            self.user_id_from_auth_method,
+            "User id from auth method is not equal to user id from check method"
+        )
+
+    @pytest.mark.parametrize('condition', exclude_params)
+    def test_negative_auth_check(self, condition):
+        if condition == "no_cookie":
+            response = requests.get(self.url_auth,
+                                    headers={'x-csrf-token': self.token}
+                                    )
+        else:
+            response = requests.get(self.url_auth,
+                                    cookies={'auth_sid': self.auth_sid}
+                                    )
+
+        Assertions.assert_json_value_by_name(
+            response,
+            "user_id",
+            0,
+            f"User is authorized with condition '{condition}'"
+        )
+
