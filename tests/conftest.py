23fa2f1972a702577a867504002d6fc6a45fb00f
Grigorii_Shakhin <grigorii_shakhin@epam.com>
2023-06-13 19:32:12 +0200

    add tests to edit user

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import pytest
 import requests
 from lib.base_case import BaseCase
 from lib.my_requests import MyRequests
+from lib.assertions import Assertions
 
 
 @pytest.fixture
@@ -12,12 +13,12 @@ def auth_user_method():
     }
     response = MyRequests.post("/user/login", data=data)
 
-    auth_sid = response.cookies["auth_sid"]
-    token = response.headers["x-csrf-token"]
-    user_id_from_auth_method = response.json()["user_id"]
-    return {"user_id": user_id_from_auth_method,
-            "x-csrf-token": token,
-            "auth_sid": auth_sid}
+    auth_sid = response.cookies['auth_sid']
+    token = response.headers['x-csrf-token']
+    user_id_from_auth_method = response.json()['user_id']
+    return {'user_id': user_id_from_auth_method,
+            'x-csrf-token': token,
+            'auth_sid': auth_sid}
 
 
 @pytest.fixture
@@ -25,15 +26,21 @@ def create_and_auth_user_method():
     data = BaseCase().prepare_registration_data()
     response = MyRequests.post("/user/", data=data)
 
+    Assertions.assert_status_code(response, 200)
+    Assertions.assert_json_has_key(response, "id")
+
     data1 = {
         'email': data['email'],
         'password': data['password']
     }
 
     response1 = MyRequests.post("/user/login", data=data1)
-    auth_sid = response1.cookies["auth_sid"]
-    token = response1.headers["x-csrf-token"]
-    user_id_from_auth_method = response1.json()["user_id"]
-    return {"user_id": user_id_from_auth_method,
-            "x-csrf-token": token,
-            "auth_sid": auth_sid}
+    auth_sid = response1.cookies['auth_sid']
+    token = response1.headers['x-csrf-token']
+    user_id_from_auth_method = response1.json()['user_id']
+    return {'user_id': user_id_from_auth_method,
+            'x-csrf-token': token,
+            'auth_sid': auth_sid,
+            'email': data1['email'],
+            'password': data1['password'],
+            'firstName': data['firstName']}
--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -3,50 +3,138 @@ from lib.assertions import Assertions
 from lib.base_case import BaseCase
 from lib.my_requests import MyRequests
 
+
 class TestUserEdit(BaseCase):
-    def test_edit_just_created_user(self):
-        register_data = self.prepare_registration_data()
-        response1 = MyRequests.post("/user/", data=register_data)
-
-        Assertions.assert_status_code(response1, 200)
-        Assertions.assert_json_has_key(response1, "id")
-
-        email = register_data['email']
-        first_name = register_data['firstName']
-        password = register_data['password']
-        user_id = self.get_json_value(response1, "id")
-
-        #LOGIN
-        login_data = {
-            'email': email,
-            'password': password
-        }
-
-        response2 = MyRequests.post("/user/login", data=login_data)
-
-        auth_sid = self.get_cookie(response2, "auth_sid")
-        token = self.get_header(response2, "x-csrf-token")
-
-        #EDIT
+    def test_edit_just_created_user(self, create_and_auth_user_method):
+        auth_data_dict = create_and_auth_user_method
+        # EDIT
         new_name = "NEW NAME"
 
-        response3 = MyRequests.put(f"/user/{user_id}",
-                                 headers={"x-csrf-token": token},
-                                 cookies={"auth_sid": auth_sid},
-                                 data={"firstName": new_name})
+        response3 = MyRequests.put(f"/user/{auth_data_dict['user_id']}",
+                                   headers={"x-csrf-token": auth_data_dict["x-csrf-token"]},
+                                   cookies={"auth_sid": auth_data_dict["auth_sid"]},
+                                   data={"firstName": new_name})
 
         Assertions.assert_status_code(response3, 200)
 
-        #GET
+        # GET
 
-        response4 = MyRequests.get(f"/user/{user_id}",
-                                 headers={"x-csrf-token": token},
-                                 cookies={"auth_sid": auth_sid}
-                                 )
+        response4 = MyRequests.get(f"/user/{auth_data_dict['user_id']}",
+                                   headers={"x-csrf-token": auth_data_dict["x-csrf-token"]},
+                                   cookies={"auth_sid": auth_data_dict["auth_sid"]}
+                                   )
 
         Assertions.assert_json_value_by_name(
             response4,
             "firstName",
             new_name,
             "Wrong name of user after edit"
+        )
+
+    def test_edit_just_created_user_as_not_auth_user(self, create_and_auth_user_method):
+        auth_data_dict = create_and_auth_user_method
+        # EDIT
+        new_name = "NEW NAME"
+
+        response = MyRequests.put(f"/user/{auth_data_dict['user_id']}",
+                                  data={"firstName": new_name})
+
+        Assertions.assert_status_code(response, 400)
+
+        # GET
+
+        response1 = MyRequests.get(f"/user/{auth_data_dict['user_id']}",
+                                   headers={"x-csrf-token": auth_data_dict["x-csrf-token"]},
+                                   cookies={"auth_sid": auth_data_dict["auth_sid"]}
+                                   )
+
+        Assertions.assert_json_value_by_name(
+            response1,
+            "firstName",
+            auth_data_dict['firstName'],
+            "Wrong name of user after edit. Name shouldn't be able to change."
+        )
+
+    def test_edit_just_created_user_auth_as_another_user(self, create_and_auth_user_method, auth_user_method):
+        edit_user_data_dict = create_and_auth_user_method
+        auth_user_data_dict = auth_user_method
+        # EDIT
+        new_name = "NEW NAME"
+
+        response = MyRequests.put(f"/user/{edit_user_data_dict['user_id']}",
+                                  data={"firstName": new_name},
+                                  headers={"x-csrf-token": auth_user_data_dict["x-csrf-token"]},
+                                  cookies={"auth_sid": auth_user_data_dict["auth_sid"]})
+
+        Assertions.assert_status_code(response, 400)
+
+        # GET
+
+        response1 = MyRequests.get(f"/user/{edit_user_data_dict['user_id']}",
+                                   headers={"x-csrf-token": edit_user_data_dict["x-csrf-token"]},
+                                   cookies={"auth_sid": edit_user_data_dict["auth_sid"]}
+                                   )
+
+        Assertions.assert_json_value_by_name(
+            response1,
+            "firstName",
+            edit_user_data_dict['firstName'],
+            "Wrong name of user after edit. Name shouldn't be able to change."
+        )
+
+    def test_edit_just_created_user_email_without_at_auth_as_same_user(self, create_and_auth_user_method):
+        edit_user_data_dict = create_and_auth_user_method
+        # EDIT
+        new_email = "".join(edit_user_data_dict['email'].split("@"))
+
+        response = MyRequests.put(f"/user/{edit_user_data_dict['user_id']}",
+                                  data={"email": new_email},
+                                  headers={"x-csrf-token": edit_user_data_dict["x-csrf-token"]},
+                                  cookies={"auth_sid": edit_user_data_dict["auth_sid"]})
+
+        Assertions.assert_status_code(response, 400)
+        Assertions.assert_content_has_text(response, "Invalid email format")
+
+        # GET
+
+        response1 = MyRequests.get(f"/user/{edit_user_data_dict['user_id']}",
+                                   headers={"x-csrf-token": edit_user_data_dict["x-csrf-token"]},
+                                   cookies={"auth_sid": edit_user_data_dict["auth_sid"]}
+                                   )
+
+        Assertions.assert_json_value_by_name(
+            response1,
+            "email",
+            edit_user_data_dict['email'],
+            "Wrong email of user after edit. Email shouldn't be able to change."
+        )
+
+    def test_edit_just_created_user_firstname_to_short_value_auth_as_same_user(self, create_and_auth_user_method):
+        edit_user_data_dict = create_and_auth_user_method
+        # EDIT
+        new_name = "X"
+
+        response = MyRequests.put(f"/user/{edit_user_data_dict['user_id']}",
+                                  data={"firstName": new_name},
+                                  headers={"x-csrf-token": edit_user_data_dict["x-csrf-token"]},
+                                  cookies={"auth_sid": edit_user_data_dict["auth_sid"]})
+
+        Assertions.assert_status_code(response, 400)
+        Assertions.assert_json_value_by_name(response,
+                                             'error',
+                                             'Too short value for field firstName',
+                                             f"Unexpexted response text {response.text}")
+
+        # GET
+
+        response1 = MyRequests.get(f"/user/{edit_user_data_dict['user_id']}",
+                                   headers={"x-csrf-token": edit_user_data_dict["x-csrf-token"]},
+                                   cookies={"auth_sid": edit_user_data_dict["auth_sid"]}
+                                   )
+
+        Assertions.assert_json_value_by_name(
+            response1,
+            "firstName",
+            edit_user_data_dict['firstName'],
+            "Wrong name of user after edit. Name shouldn't be able to change."
         )
